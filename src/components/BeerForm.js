fa3850d601902bd7b31521e21ee312e76a563f03
Anthony DiFalco <aodifalco@gmail.com>
2022-09-09 09:58:31 -0700

    add beerform

---
+++ src/components/BeerForm.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { v4 } from "uuid";
+import PropTypes from "prop-types";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+function BeerForm(props) {
+
+  function handleBeerFormSubmission(event) {
+    event.preventDefault();
+    props.onNewBeerCreation({
+      Name: event.target.name.value,
+      Type: event.target.type.value,
+      Walls: event.target.quantity.value,
+      Bottles: event.target.quantity.value * 99
+    });
+  }
+
+  return (
+    <Form onSubmit={handleBeerFormSubmission}>
+      <Form.group controlId="name">
+        <Form.Label>Drink Name</Form.Label>
+        <Form.Control type="text" />
+      </Form.group>
+      <Form.Group>
+        <Form.Label>Drink Type</Form.Label>
+        <Form.Control type="text" />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Quantity</Form.Label>
+        <Form.Control type="number" />
+      </Form.Group>
+    </Form>
+  );
+}
+
+BeerForm.propTypes = {
+  onNewBeerCreation: PropTypes.func
+}
+
+export default BeerForm;
